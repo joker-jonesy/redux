beb7a14e17712fa90ffb8718203a3932d2fac231
Luke Jones <spashmcgrath94@yahoo.com>
2020-02-26 14:39:02 -0500

    More stuff

--- src/components/firebaseProducts/FireBaseProducts.js
+++ src/components/firebaseProducts/FireBaseProducts.js
@@ -6,34 +6,28 @@ import fire from "../../firebase/Fire";
 function FirebaseProducts(props) {
     const [items, setItems] = React.useState([]);
     const user = useSelector(state => state.user);
-    const cart = useSelector(state => state.cart);
-    const sort = useSelector(state => state.sort);
     const [country] = React.useState(user.country);
-    const dispatch = useDispatch();
+    const [submitCheck, checkSubmit]=React.useState(false);
+    const [value, setValues] = React.useState({
+        color: "",
+        cost: {
+            USA: "",
+            CANADA: "",
+            UK: ""
+        },
+        name: "",
+        type: ""
+    });
 
-    // React.useEffect(()=>{
-    //     let sortedList = cart;
-    //     for(let i in sort){
-    //         if(sort[i].toggle){
-    //             sortedList=sortedList.filter(it=>{
-    //                 return sort[i].val.includes(it[i])
-    //             });
-    //         }
-    //     }
-    //
-    //
-    //     setItems(sortedList);
-    // },[sort, cart]);
 
     const db = fire.firestore();
 
-    React.useEffect(()=> {
+    React.useEffect(() => {
         let newItems = [];
 
         db.collection("products").get().then(function (snapshot) {
             snapshot.forEach(function (doc) {
                 const object = doc.data();
-                console.log(doc.data().name);
 
                 let item = {
                     color: object.color,
@@ -43,16 +37,59 @@ function FirebaseProducts(props) {
                         UK: object.cost.UK
                     },
                     name: object.name,
-                    type: object.type
+                    type: object.type,
+                    id:doc.id
                 };
 
                 newItems.push(item);
             });
+
+            setItems(newItems);
+            checkSubmit(false);
         });
+    }, [db, submitCheck]);
 
-        setItems(newItems);
+    const handleChange = prop => event => {
+        if (prop === "cost") {
+            setValues({
+                ...value, [prop]: {
+                    USA: Number(event.target.value),
+                    CANADA: Number(event.target.value)+5,
+                    UK: Number(event.target.value)/2
 
-    }, [cart]);
+                }
+            });
+        } else {
+            setValues({...value, [prop]: event.target.value});
+        }
+
+    };
+
+    const submit =()=>{
+        if(!isNaN(value.cost.USA)){
+            db.collection("products").add(value).then(()=>{
+                    setValues({
+                        color: "",
+                        cost: {
+                            USA: "",
+                            CANADA: "",
+                            UK: ""
+                        },
+                        name: "",
+                        type: ""
+                    });
+                    checkSubmit(true);
+            });
+
+        }
+
+    };
+
+    const deleteItem =(id)=>{
+        db.collection("products").doc(id).delete().then(()=>{
+            checkSubmit(true);
+        })
+    };
 
 
     let itemsEle = items.map((it, idx) =>
@@ -62,6 +99,7 @@ function FirebaseProducts(props) {
             <h4>{country === "USA" && "Cost in USA:" + it.cost.USA}</h4>
             <h4>{country === "CANADA" && "Cost in CANADA:" + it.cost.CANADA}</h4>
             <h4>{country === "UK" && "Cost in UK:" + it.cost.UK}</h4>
+            <button onClick={()=>{deleteItem(it.id)}}>Delete Me</button>
         </div>
     );
 
@@ -71,13 +109,13 @@ function FirebaseProducts(props) {
 
     return (
         <div>
-            <button style={sort.type.val.includes("shirt") ? style : null}
-                    onClick={() => dispatch(changeSort("type", "shirt"))}>Shirts
-            </button>
-            <button onClick={() => dispatch(changeSort("type", "pants"))}>Pants</button>
-            <button onClick={() => dispatch(changeSort("color", "red"))}>Red</button>
-            <button onClick={() => dispatch(changeSort("color", "green"))}>Green</button>
-            <button onClick={() => dispatch(changeSort("color", "blue"))}>Blue</button>
+            <div>
+                <input type="text" placeholder="Name" onChange={handleChange("name")} value={value.name}/>
+                <input type="text" placeholder="Cost" onChange={handleChange("cost")} value={value.cost.USA}/>
+                <input type="text" placeholder="Type" onChange={handleChange("type")} value={value.type}/>
+                <input type="text" placeholder="Color" onChange={handleChange("color")} value={value.color}/>
+                <button onClick={submit}>Submit</button>
+            </div>
             {items.length === 0 ? "No Items Filtered" : itemsEle}
         </div>
     )
